e0f1548a7fe69b7b1fef9e6171b2371aaf9ef1bf
Nghia The Pham <phamthenghia1985@gmail.com>
2015-11-06 17:31:27 -0800

    - nothing to lose

---
+++ gradient_check_attention_pair.lua
@@ -0,0 +1,46 @@
+
+local function parse_opt()
+  local cmd = torch.CmdLine()
+  cmd:text()
+  cmd:text('Train a character-level language model')
+  cmd:text()
+  cmd:text('Options')
+  -- data
+  cmd:option('-data_dir','data/tinyshakespeare','data directory. Should contain the file input.txt with input data')
+  -- model params
+  cmd:option('-rnn_size', 128, 'size of LSTM internal state')
+  cmd:option('-num_layers', 2, 'number of layers in the LSTM')
+  cmd:option('-model', 'lstm', 'lstm,gru or rnn')
+  -- optimization
+  cmd:option('-learning_rate',2e-3,'learning rate')
+  cmd:option('-learning_rate_decay',0.97,'learning rate decay')
+  cmd:option('-learning_rate_decay_after',10,'in number of epochs, when to start decaying the learning rate')
+  cmd:option('-decay_rate',0.95,'decay rate for rmsprop')
+  cmd:option('-dropout',0,'dropout for regularization, used after each RNN hidden layer. 0 = no dropout')
+  cmd:option('-seq_length',50,'number of timesteps to unroll for')
+  ---- TODO set batch size > 1 to gradient check batch later
+  -- this time 1 to check simple first
+  cmd:option('-batch_size',1,'number of sequences to train on in parallel')
+  cmd:option('-max_epochs',50,'number of full passes through the training data')
+  cmd:option('-grad_clip',5,'clip gradients at this value')
+  cmd:option('-train_frac',0.95,'fraction of data that goes into train set')
+  cmd:option('-val_frac',0.05,'fraction of data that goes into validation set')
+              -- test_frac will be computed as (1 - train_frac - val_frac)
+  cmd:option('-init_from', '', 'initialize network parameters from checkpoint at this path')
+  -- bookkeeping
+  cmd:option('-seed',123,'torch manual random number generator seed')
+  cmd:option('-print_every',1,'how many steps/minibatches between printing out the loss')
+  cmd:option('-eval_val_every',1000,'every how many iterations should we evaluate on validation data?')
+  cmd:option('-checkpoint_dir', 'cv', 'output directory where checkpoints get written')
+  cmd:option('-savefile','lstm','filename to autosave the checkpont to. Will be inside checkpoint_dir/')
+  -- GPU/CPU
+  cmd:option('-gpuid',0,'which gpu to use. -1 = use CPU')
+  cmd:option('-opencl',0,'use OpenCL (instead of CUDA)')
+  cmd:text()
+  
+  -- parse input params
+  local opt = cmd:parse(arg)
+  return opt
+end
+
+local opt = parse_opt()
--- model/nghia_util.lua
+++ model/nghia_util.lua
@@ -1,5 +1,17 @@
 local FakeLoader = {}
 
-FakeLoader.next
+function FakeLoader:next_batch()
+    x = {torch.Tensor{1,2,3,5,6},torch.Tensor{2,5}}
+    y = {torch.Tensor{3}}
+    return {x,y}
+end
+
+local FakeBatchLoader = {}
+-- TODO:
+function FakeBatchLoader:next_batch()
+    x = {{1,2,3,5,6},{2,5}}
+    y = {3}
+    return {x,y}
+end
 
 local RealLoader = {}
